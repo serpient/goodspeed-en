2d425ba0dde50859232e09d143ec6efc938cfe40
serpient <littlegangwolf@gmail.com>
2020-06-18 10:33:46 -0700

    Updates

---
+++ precache-manifest.4c80d71397eb2bc73212eaf97b3645e2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9dc906f7a436b0aeaad65dbba4fb8f",
+    "url": "/goodspeed-en/index.html"
+  },
+  {
+    "revision": "54fff18a32d67c072162",
+    "url": "/goodspeed-en/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "62e6b48798e3a4c00b07",
+    "url": "/goodspeed-en/static/css/main.0e697cfa.chunk.css"
+  },
+  {
+    "revision": "54fff18a32d67c072162",
+    "url": "/goodspeed-en/static/js/2.c2a17fa6.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/goodspeed-en/static/js/2.c2a17fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e6b48798e3a4c00b07",
+    "url": "/goodspeed-en/static/js/main.566ba4ad.chunk.js"
+  },
+  {
+    "revision": "7ba346d5559a401f9384",
+    "url": "/goodspeed-en/static/js/runtime-main.860b2553.js"
+  },
+  {
+    "revision": "8372aa1b8193d23cc9a2951965320fbc",
+    "url": "/goodspeed-en/static/media/4K-HDTV-relative-sizes8.8372aa1b.jpg"
+  },
+  {
+    "revision": "2c2f6b1a0f82c062208c6163dc5a0235",
+    "url": "/goodspeed-en/static/media/citi_private.2c2f6b1a.jpg"
+  },
+  {
+    "revision": "037867e5a5d435be80c12a9cc26bab48",
+    "url": "/goodspeed-en/static/media/cnn.037867e5.jpg"
+  },
+  {
+    "revision": "201896c75d77e8a3f731f740b2d1bfdc",
+    "url": "/goodspeed-en/static/media/discovery_channel.201896c7.jpg"
+  },
+  {
+    "revision": "9820e1673b65824b4e622d93d428ceb2",
+    "url": "/goodspeed-en/static/media/f07.9820e167.jpg"
+  },
+  {
+    "revision": "984b6d684857ad0ad982f56f1179357f",
+    "url": "/goodspeed-en/static/media/f09.984b6d68.jpg"
+  },
+  {
+    "revision": "ac7211f3c6b92da01540719b8458df58",
+    "url": "/goodspeed-en/static/media/f10.ac7211f3.jpg"
+  },
+  {
+    "revision": "e97664b9b2493be7b8a78ab1e03a6351",
+    "url": "/goodspeed-en/static/media/f11.e97664b9.jpg"
+  },
+  {
+    "revision": "a29591d3f4845690da720f8f5903e354",
+    "url": "/goodspeed-en/static/media/goodspeedfilm_logo1.a29591d3.gif"
+  },
+  {
+    "revision": "187e070fb436c517160d97f2e42454e9",
+    "url": "/goodspeed-en/static/media/hero-1.187e070f.jpg"
+  },
+  {
+    "revision": "abbc38cff55c36f392d5889ce1afe21a",
+    "url": "/goodspeed-en/static/media/hero-2.abbc38cf.jpg"
+  },
+  {
+    "revision": "dedd1a2a030d54c2c38ca0fdd644e723",
+    "url": "/goodspeed-en/static/media/ministry_of_culture.dedd1a2a.jpg"
+  },
+  {
+    "revision": "b4be6c9217e8e7ec054537e6fa446b0f",
+    "url": "/goodspeed-en/static/media/national_center_for_traditional_arts.b4be6c92.png"
+  },
+  {
+    "revision": "67f534d8b0947b8062f215e31ef6b851",
+    "url": "/goodspeed-en/static/media/national_central_library.67f534d8.png"
+  },
+  {
+    "revision": "8226d46397bb60ef780b6b818ac80520",
+    "url": "/goodspeed-en/static/media/national_geographic_center.8226d463.png"
+  },
+  {
+    "revision": "3213fd09d68b2a03c3eaabdd5e736ad8",
+    "url": "/goodspeed-en/static/media/national_taiwan_library.3213fd09.png"
+  }
+]);
